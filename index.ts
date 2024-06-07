4222c78e7b899644103da7d5bfca85098b572f14
Steve Nolte <steve.nolte@bestow.com>
2024-06-07 16:04:38 -0500

    Use a sweet API service

--- index.ts
+++ index.ts
@@ -1,14 +1,77 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
+import * as postgres from "@pulumi/postgresql";
 
-const appLabels = { app: "nginx" };
-const deployment = new k8s.apps.v1.Deployment("nginx", {
+// Define the PostgreSQL deployment
+const postgresDeployment = new k8s.apps.v1.Deployment("postgres-deployment", {
     spec: {
-        selector: { matchLabels: appLabels },
+        selector: { matchLabels: { app: "postgres" } },
         replicas: 1,
         template: {
-            metadata: { labels: appLabels },
-            spec: { containers: [{ name: "nginx", image: "nginx" }] }
-        }
-    }
+            metadata: { labels: { app: "postgres" } },
+            spec: {
+                containers: [
+                    {
+                        name: "postgres",
+                        image: "postgres:latest",
+                        ports: [{ containerPort: 5432 }],
+                        env: [
+                            { name: "POSTGRES_USER", value: "admin" },
+                            { name: "POSTGRES_PASSWORD", value: "password" },
+                            { name: "POSTGRES_DB", value: "mydb" },
+                        ],
+                    },
+                ],
+            },
+        },
+    },
 });
-export const name = deployment.metadata.name;
+
+// Define the PostgreSQL service
+const postgresService = new k8s.core.v1.Service("postgres-service", {
+    spec: {
+        selector: { app: "postgres" },
+        ports: [{ port: 5432, targetPort: 5432 }],
+    },
+});
+
+// Define the REST API deployment
+const apiDeployment = new k8s.apps.v1.Deployment("api-deployment", {
+    spec: {
+        selector: { matchLabels: { app: "api" } },
+        replicas: 1,
+        template: {
+            metadata: { labels: { app: "api" } },
+            spec: {
+                containers: [
+                    {
+                        name: "api",
+                        image: "my-rest-api", // Replace with your API Docker image
+			imagePullPolicy: "Never",
+                        ports: [{ containerPort: 3000 }],
+                        env: [
+                            { name: "DATABASE_HOST", value: postgresService.metadata.name },
+                            { name: "DATABASE_PORT", value: "5432" },
+                            { name: "DATABASE_USER", value: "admin" },
+                            { name: "DATABASE_PASSWORD", value: "password" },
+                            { name: "DATABASE_NAME", value: "mydb" },
+                        ],
+                    },
+                ],
+            },
+        },
+    },
+});
+
+// Define the REST API service
+const apiService = new k8s.core.v1.Service("api-service", {
+    spec: {
+        selector: { app: "api" },
+        ports: [{ port: 3000, targetPort: 3000 }],
+        type: "LoadBalancer",
+    },
+});
+
+// Export the API service URL
+export const apiUrl = apiService.status.loadBalancer.ingress[0].ip;
+
--- start.sh
+++ start.sh
@@ -8,5 +8,10 @@ pulumi login --local
 
 pulumi new kubernetes-typescript --force
 
+# https://www.pulumi.com/registry/packages/kubernetes/how-to-guides/kubernetes-ts-exposed-deployment/
+pulumi config set isMinikube true
+
 pulumi up
+
+npm install @pulumi/kubernetes @pulumi/postgresql
 
